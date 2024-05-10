6e9e66f328d860c83f8162633f468d6eafede3b2
Maria Khan <mariapervaiz40@gmail.com>
2024-05-10 15:58:28 +0500

    first commit

---
+++ program.js
@@ -0,0 +1,22 @@
+import { Student } from "./student.js";
+import inquirer from 'inquirer';
+import chalk from "chalk";
+console.log(chalk.bold.yellow("  -------- Welcome --------\n"));
+const { num } = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'num',
+        message: chalk.magenta('Type 1 If You Like To Talk To Others And Type 2 If You Would Rather Keep It To Yourself: ')
+    }
+]);
+const MyStudent = new Student();
+MyStudent.AskQuestion(num);
+console.log(`You are: ${MyStudent.GetPersonality()}`);
+const { name } = await inquirer.prompt([
+    {
+        name: 'name',
+        message: 'Enter Your Name: '
+    }
+]);
+MyStudent.Name = name;
+console.log(`Your Name is: ${MyStudent.Name} and your personality is: ${MyStudent.GetPersonality()} `);
---
+++ program.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import { Student } from "./student.js";
+
+import inquirer from 'inquirer';
+import chalk from "chalk";
+
+
+console.log(chalk.bold.yellow("  -------- Welcome --------\n"));
+
+const { num }: { num: number } = await inquirer.prompt([
+    {
+    type: 'number',
+    name: 'num',
+    message: chalk.magenta('Type 1 If You Like To Talk To Others And Type 2 If You Would Rather Keep It To Yourself: ')
+}
+]);
+
+const MyStudent = new Student()
+MyStudent.AskQuestion(num)
+console.log(`You are: ${MyStudent.GetPersonality()}`);
+
+
+const { name }: { name: string } = await inquirer.prompt([
+    {
+    name: 'name',
+    message: 'Enter Your Name: '
+}
+]);
+
+MyStudent.Name = name
+console.log(`Your Name is: ${MyStudent.Name} and your personality is: ${MyStudent.GetPersonality()} `);
